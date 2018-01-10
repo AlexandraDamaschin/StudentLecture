34067a5a7135a0d97e5046b83b106767ed94407c
Alexandra Damaschin <damaschin.alexandra.maria@gmail.com>
2018-01-10 15:29:47 +0000

    seedUsers and seedRoles

--- StudentLecture/Migrations/ApplicationUsersMigrations/Configuration.cs
+++ StudentLecture/Migrations/ApplicationUsersMigrations/Configuration.cs
@@ -1,11 +1,17 @@
+
 namespace StudentLecture.Migrations.ApplicationUsersMigrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System.Collections.Generic;
+    using StudentLecture.Models;
+    using StudentLecture.Models.StudentLectureModels;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<StudentLecture.Models.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<Models.ApplicationDbContext>
     {
         public Configuration()
         {
@@ -13,7 +19,7 @@ namespace StudentLecture.Migrations.ApplicationUsersMigrations
             MigrationsDirectory = @"Migrations/ApplicationUsersMigrations";
         }
 
-        protected override void Seed(StudentLecture.Models.ApplicationDbContext context)
+        protected override void Seed(Models.ApplicationDbContext context)
         {
             //  This method will be called after migrating to the latest version.
 
@@ -27,6 +33,70 @@ namespace StudentLecture.Migrations.ApplicationUsersMigrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+            SeedUsers(context);
+            SeedRoles(context);
+        }
+
+        private void SeedRoles(ApplicationDbContext context)
+        {
+
+            var manager =
+             new UserManager<ApplicationUser>(
+                 new UserStore<ApplicationUser>(context));
+
+            var roleManager =
+                new RoleManager<IdentityRole>(
+                    new RoleStore<IdentityRole>(context));
+
+            //create roles
+            roleManager.Create(new IdentityRole { Name = "Lecture" });
+            roleManager.Create(new IdentityRole { Name = "Student" });
+
+            //asign roles to users
+            ApplicationUser lecture = manager.FindByEmail("john.k@itsligo.ie");
+            if (lecture != null)
+            {
+                manager.AddToRoles(lecture.Id, new string[] { "Lecture" });
+            }
+            else
+            {
+                throw new Exception { Source = "Did not find lecture" };
+            }
+
+            ApplicationUser student = manager.FindByEmail("S01@itsligo.ie");
+            if (student != null)
+            {
+                manager.AddToRoles(student.Id, new string[] { "Student" });
+            }
+            else
+            {
+                throw new Exception { Source = "Did not find student" };
+            }
+            context.SaveChanges();
+        }
+
+        private void SeedUsers(ApplicationDbContext context)
+        {
+            //seeding two applicationUsers
+            context.Users.AddOrUpdate(u => u.Email, new ApplicationUser
+            {
+                Email = "john.k@itsligo.ie",
+                EmailConfirmed = true,
+                UserName = "john.k@itsligo.ie",
+                PasswordHash = new PasswordHasher().HashPassword("ITSligo$1"),
+                SecurityStamp = Guid.NewGuid().ToString(),
+            });
+
+            context.Users.AddOrUpdate(u => u.Email, new ApplicationUser
+            {
+                Email = "S01@itsligo.ie",
+                EmailConfirmed = true,
+                UserName = "S01@itsligo.ie",
+                PasswordHash = new PasswordHasher().HashPassword("ITSligo$2"),
+                SecurityStamp = Guid.NewGuid().ToString(),
+            });
+            context.SaveChanges();
         }
     }
 }
